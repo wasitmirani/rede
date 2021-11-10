950826ad621f5d44d32b7e5403d833e68b7e7751
HaseebHassan <mhhassan095@gmail.com>
2021-11-09 16:49:48 -0800

    new changes added

--- resources/views/frontend/pages/profile.blade.php
+++ resources/views/frontend/pages/profile.blade.php
@@ -10,13 +10,9 @@
         <li><a href="#tab2">Interest</a></li>
         <li><a href="#tab3">Event</a></li>
         <li><a href="#tab4">Group</a></li>
-
     </ul>
   <div class="grid lg:grid-cols-3 mt-12 gap-8 tab-content " id="tab1">
-
         <div class="bg-white rounded-md lg:shadow-lg shadow col-span-2">
-
-
             <div class="bg-red-900 text-center py-4 lg:px-4">
               <div class="p-2 bg-red-800 items-center text-red-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
                 <span class="flex rounded-full bg-red-500 uppercase px-2 py-1 text-xs font-bold mr-3"></span></span>
@@ -24,8 +20,6 @@
                 <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
               </div>
             </div>
-
-
     <form action="{{route('edit.profile')}}" method="post" enctype="multipart/form-data">
     @csrf
     <div class="grid grid-cols-2 gap-3 lg:p-6 p-4">
