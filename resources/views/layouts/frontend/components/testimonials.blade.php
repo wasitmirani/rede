8941a54c8438f326a7c75bb9d0118e65b9ee45ba
HaseebHassan <mhhassan095@gmail.com>
2021-12-15 08:33:13 -0800

    new chnages

--- resources/views/layouts/frontend/components/testimonials.blade.php
+++ resources/views/layouts/frontend/components/testimonials.blade.php
@@ -6,20 +6,27 @@
 				<div class="box">
 					<div class="img-box"><img src="/assets/images/client-img.png" alt=""></div>
 					<div class="comma"><img src="/assets/images/comma.png" alt=""></div>
-					<p>“it`s good to have an end to journey
-						toward; but it is the journey
-					that matters, in the end,”</p>
-					<h6>- Ernest hemingway <strong>Novelist</strong></h6>
+				
+					<p>“Friendship is born at that 
+						moment when one person 
+						says to another: ‘What! You 
+						too? I thought I was the only 
+						one.”</p>
+					<h6>- C.S. Lewis,  <strong>Novelist</strong></h6>
 				</div>
 			</div>
 			<div class="item">
 				<div class="box">
 					<div class="img-box"><img src="/assets/images/client-img.png" alt=""></div>
 					<div class="comma"><img src="/assets/images/comma.png" alt=""></div>
-					<p>“it`s good to have an end to journey
-						toward; but it is the journey
-					that matters, in the end,”</p>
-					<h6>- Ernest hemingway <strong>Novelist</strong></h6>
+					<p>“Ernie:      “Would you still like 
+						me if I               wore fruit on 
+						my head?”
+						Bert: “Yes, Ernie... I 
+						would still like you.
+						 ”</p>
+					<h6>
+						- Sesame Street <strong></strong></h6>
 				</div>
 			</div>
 			<div class="item">
